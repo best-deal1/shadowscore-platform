dbb72ad53134626604ab31ba22625d9ee550beaa
best-deal1 <83040959+best-deal1@users.noreply.github.com>
2026-07-06 16:05:13 +0300

    Wire DNS provider into defaults

--- lib/providers/defaultProviders.ts
+++ lib/providers/defaultProviders.ts
@@ -1,12 +1,11 @@
+import { DNSProvider } from "./DNSProvider";
 import { WHOISProvider } from "./WHOISProvider";
 import { createPlaceholderProviders } from "./placeholderProviders";
 
 export function createDefaultProviders() {
-  const placeholderProviders = createPlaceholderProviders();
-
   return [
-    ...placeholderProviders.slice(0, 2),
+    ...createPlaceholderProviders(),
+    new DNSProvider(),
     new WHOISProvider(),
-    ...placeholderProviders.slice(2),
   ];
 }
