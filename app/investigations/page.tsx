7cb4a239d382f29636fd0707db6086a9ca8964a0
best-deal1 <83040959+best-deal1@users.noreply.github.com>
2026-07-20 17:40:51 +0300

    Add investigation workflow workspace

--- app/investigations/page.tsx
+++ app/investigations/page.tsx
@@ -1,1 +1,7 @@
-export { default } from "../intake/page";
+import { investigationWorkflowService } from "@/lib/investigation/workflowService";
+import { InvestigationWorkspace } from "./_components/InvestigationWorkspace";
+
+export default async function InvestigationsPage() {
+  const investigations = await investigationWorkflowService.list();
+  return <InvestigationWorkspace initialInvestigations={investigations} />;
+}
