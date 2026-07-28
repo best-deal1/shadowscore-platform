1e2a46804d7117f81b8b104a427b4715e52d5831
best-deal1 <83040959+best-deal1@users.noreply.github.com>
2026-07-28 18:17:12 +0300

    Add SEC authoritative entity resolver

--- lib/entityResolution/authoritative/sec/index.ts
+++ lib/entityResolution/authoritative/sec/index.ts
@@ -1,4 +1,7 @@
 export { SECClient, SEC_URLS, normalizeCIK } from "./client";
 export type { SECClientOptions } from "./client";
 export { SECRegistry } from "./registry";
+export { SECAuthoritativeResolver, SEC_AUTHORITATIVE_RESOLVER_VERSION } from "./resolver";
+export type { SECAuthoritativeResolverOptions } from "./resolver";
+export type { SECResolutionCandidate, SECResolutionResult } from "./resolutionTypes";
 export type { SECCompanyTickerDataset, SECCompanyTickerRow, SECSubmissions } from "./types";
